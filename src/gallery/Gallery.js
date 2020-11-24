f0de7fb7643aae42c3290c0f3645a0514d441455
deadlock <mhfred.deadlock@gmail.com>
2020-11-24 12:50:36 +0800

    update event

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
 const Level120Gallery = () => {
     // const classes = useStyles();
     const images = [];
-    for (let i = 1; i <= 9; i++) {
+    for (let i = 1; i <= 8; i++) {
         images.push(
             {
                 original: '/photos/lvl120/original/lvl120.'+i+'.jpg',
