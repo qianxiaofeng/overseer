bf6afa63053ec7dbb32fed729e5b8f82622b9979
deadlock <mhfred.deadlock@gmail.com>
2020-11-28 18:18:02 +0800

    recruit add ribbon

--- src/home/Recruit.js
+++ src/home/Recruit.js
@@ -24,56 +24,41 @@ const useStyles = makeStyles((theme) => ({
         color: "#f1f6f9",
         opacity: 0.75,
         textAlign: "center",
-        position:"relative",
-        overflow:"hidden"
-    },
-    gridEmpty: {
-        backgroundColor: "#9ba4b4",
-        opacity: 0.5,
+        position: "relative",
+        overflow: "hidden"
     },
     gridList: {
         width: "100%",
         height: "auto",
     },
-    title: {
-        paddingTop: '25px',
-    },
-    question: {
-        height: 120,
-        width: 120,
-        position: "relative",
-        top: 180,
-    },
+
     ribbon: {
-        margin: 0,
-        padding:0,
-        background: "rebeccapurple",
-        color: "white",
-        // padding: "1 0",
+        top: -30,
+        right: -30,
+        // marginLeft:"45%",
+        width: 150,
+        height: 150,
+        overflow: "hidden",
         position: "absolute",
-        top: 0,
-        right: 0,
-        transform: "translateX(30%) translateY(0%) rotate(45deg)",
-        transformOrigin: "top left",
-        "&:before":{
-            right:"100%",
-            content: '',
+        "&::before, &::after": {
             position: "absolute",
-            top:0,
-            margin: "0 -1px", /* tweak */
-            width: "100%",
-            height: "100%",
+            zIndex: -1,
+            content: '',
+            display: 'block',
             background: "rebeccapurple",
         },
-        "&:after":{
-            left:"100%",
-            content: '',
-            position: "absolute",
-            top:0,
-            margin: "0 -1px", /* tweak */
-            width: "100%",
-            height: "100%",
-            background: "rebeccapurple",
+        "& span": {
+            position: 'absolute',
+            display: 'block',
+            width: 225,
+            padding: "15 0",
+            left: -25,
+            top: 30,
+            transform: "rotate(45deg)",
+            backgroundColor: "rebeccapurple",
+            color: "#fff",
+            textShadow: "0 1 1 rgba(0,0,0,.2)",
+            textAlign: "center",
         },
     }
 }));
@@ -81,11 +66,11 @@ const useStyles = makeStyles((theme) => ({
 
 const RecruitCard = (props) => {
     const classes = useStyles();
-    const {name, time, content, style} = props;
+    const {name, time, content, isPro = false} = props;
 
     return (
-        <Card className={style}>
-            {/*<h1 className={classes.ribbon}>test</h1>*/}
+        <Card className={classes.grid}>
+            {isPro && <h3 className={classes.ribbon}><span>进度团</span></h3>}
             <CardContent>
                 <h1>{name}</h1>
                 <h2>{time}</h2>
@@ -122,36 +107,46 @@ export default function Recruit() {
         <div className={classes.root}>
             <GridList cellHeight={300} className={classes.gridList} cols={2}>
                 <GridListTile cols={1}>
-                    <RecruitCard style={classes.grid} name={"树色 | 晚间一团"}
-                                 time={"每周两天，服务器时间周六日晚上21:00-23:00。(海外玩家请自行换算时差)"} content={"(开荒期间会有加班) 全职业招募。"}/>
+                    <RecruitCard name={"树色 | 晚间一团"}
+                                 time={"每周两天，服务器时间周六日晚上21:00-23:00。(海外玩家请自行换算时差)"}
+                                 content={"(开荒期间会有加班) 全职业招募。"}
+                    />
                 </GridListTile>
                 <GridListTile cols={1}>
-                    <RecruitCard style={classes.grid} name={"树色 | 晚间二团"}
+                    <RecruitCard name={"树色 | 晚间二团"}
                                  time={"每周三天，服务器时间周四六日晚上20:00-23:00。(海外玩家请自行换算时差)"}
-                                 content={"(开荒期间会有加班) 进度团，全职业招募强力玩家。"}/>
+                                 content={"(开荒期间会有加班) 全职业招募强力玩家。"}
+                                 isPro={true}
+                    />
                 </GridListTile>
             </GridList>
             <GridList cellHeight={300} className={classes.gridList} cols={2}>
                 <GridListTile cols={1}>
-                    <RecruitCard style={classes.grid} name={"美西 | 白天一团"}
-                                 time={"每周两天 服务器时间周六日上午9:00-11：00。(海外玩家请自行换算时差)"} content={"(开荒期间会有加班)"}/>*/}
-
+                    <RecruitCard name={"美西 | 白天一团"}
+                                 time={"每周两天 服务器时间周六日上午9:00-11：00。(海外玩家请自行换算时差)"}
+                                 content={"(开荒期间会有加班)"}
+                    />
                 </GridListTile>
                 <GridListTile cols={1}>
-                    <RecruitCard style={classes.grid} name={"美西 | 白天二团"}
+                    <RecruitCard name={"美西 | 白天二团"}
                                  time={"每周三天 服务器时间周五六日上午8:30-11：30。(海外玩家请自行换算时差)"}
-                                 content={"(开荒期间会有加班) 进度团，全职业招募强力玩家。"}/>*/}
+                                 content={"(开荒期间会有加班) 全职业招募强力玩家。"}
+                                 isPro={true}
+                    />
                 </GridListTile>
             </GridList>
             <GridList cellHeight={300} className={classes.gridList} cols={2}>
                 <GridListTile cols={1}>
-                    <RecruitCard style={classes.grid} name={"午夜 | 欧洲团"}
-                                 time={"每周三天，服务器时间周二三四(暂定)凌晨2：00-5：00。(海外玩家请自行换算时差)"} content={"(开荒期间会有加班) 全职业招募。"}/>
+                    <RecruitCard name={"午夜 | 欧洲团"}
+                                 time={"每周三天，服务器时间周二三四(暂定)凌晨2：00-5：00。(海外玩家请自行换算时差)"}
+                                 content={"(开荒期间会有加班) 全职业招募。"}
+                    />
                 </GridListTile>
                 <GridListTile cols={1}>
-                    <RecruitCard style={classes.grid} name={"闲园 | 下午团"}
+                    <RecruitCard name={"闲园 | 下午团"}
                                  time={"每周三天，服务器时间周四一二下午14:00-17:00。(海外玩家请自行换算时差)"}
-                                 content={"(加班期间为四天) 招募指挥，招募强力玩家。"}/>
+                                 content={"(加班期间为四天) 招募指挥，招募强力玩家。"}
+                    />
                 </GridListTile>
             </GridList>
         </div>
