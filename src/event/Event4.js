52001cccebd85d5575ba0eabff8616e1c45ca5e8
deadlock <mhfred.deadlock@gmail.com>
2021-03-08 19:49:34 +0800

    update pvp event

--- src/event/Event4.js
+++ src/event/Event4.js
@@ -6,6 +6,32 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import Container from '@material-ui/core/Container';
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from "react-image-gallery";
+
+
+const Rules = () => {
+    const images = [];
+    const filenames=["68991615181672_","69001615181674_","69011615181676_","69031615181678_","69041615181679_","69051615181681_","69061615181683_"]
+    for (let i = 0; i < filenames.length; i++) {
+        images.push(
+            {
+                original: '/photos/event_pvp/' + filenames[i] + '.pic.jpg',
+                thumbnail: '/photos/event_pvp/' + filenames[i] + '.pic_thumb.jpg',
+            }
+        )
+    }
+    return (
+        <ImageGallery
+            items={images}
+            lazyLoad={false}
+            thumbnailPosition={"top"}
+            autoPlay={false}
+            showIndex={true}
+            infinite={false}
+        />
+    );
+}
 
 const useStyles = makeStyles((theme) => ({
     carousel: {
@@ -33,9 +59,7 @@ const useStyles = makeStyles((theme) => ({
 const content = `
 # 细水第一届暨二月二抬头节PK插旗大赛
 
-## 大家千呼万唤（并没有）的插旗大赛终于要开始了！经活动策划群亚美欧三地管理掰着脚趾头算时差研究了一晚上，终于确定在三月13或14号（最终时间看报名表结果来确定）举行。地点在奥格大门口高调进行！
-
-### 参加人数在10人及以下，实行个人1V1赛制，最终获胜者为总冠军；参加人数超过10人则分组两两对抗最终产生冠军。所有参赛人员均可参加抽奖环节，采用R点砸金蛋的方式随机获得金币、坐骑或小宠物等不同奖励。更有稀有宠物坐骑等你来领取哦！
+## 大家千呼万唤（并没有）的 PVP 大赛终于要开始了！经活动策划群亚美欧三地管理掰着脚趾头算时差研究了一晚上，终于确定在三月13号晚上23:00举行。
 
 ## 个人总冠军奖金10万G
 
@@ -44,6 +68,8 @@ const content = `
 ### 我们虽然是PVE向的公会，但重在参与，更为了展示（嘚瑟）我们公会的风采（顺便再收收人扬扬名）！（鉴于春节合影期间顺利收到新人一枚，此次活动希望能收到喜欢PVP的玩家，增加公会PVP的热情）整个比赛由会长斗鱼直播（不要刷礼物不要刷礼物非要刷礼物的麻烦换成金币捐公会银行呀！）、同时比赛结果会放在公会网站和NGA活动板块展示。
 
 ## 欢迎团队或个人对本次活动进行赞助，活动会对赞助者冠名宣传哦！（哈哈）
+
+# 规则详情见下方图片
 `
 
 function Gallery() {
@@ -55,6 +81,7 @@ function Gallery() {
             <Container maxWidth="md">
                 <ReactMarkdown plugins={[gfm]} className={classes.content}>{content}</ReactMarkdown>
             </Container>
+            <Rules/>
             <Footer/>
         </>
     )
